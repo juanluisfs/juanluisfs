4d54870b16eb33f0c540cbc10ff5f7a0f74c8839
Juan Luis Flores <juluflosa01.demos@gmail.com>
2024-10-16 20:53:49 -0600

    Update README.swift

--- README.swift
+++ README.swift
@@ -12,7 +12,8 @@ struct ContentView: View {
   let age = 22
   let county = "🇲🇽"
   var interests = ["Swift", "Apple", "MATLAB", "Python", "Design Thinking"]
-  var awards = ["Swift Student Challenge 2024". "StartUpShip 2023", "Expo Ingenierias 2022", "Conexion Tec 2020", "PrepApps 2019", "Health Innovation Fair 2018". "Mexican Mathematics Olympiad 2014"]
+  var awards = ["Swift Student Challenge 2024". "StartUpShip 2023", "Expo Ingenierias 2022", "Conexion Tec 2020", 
+                "PrepApps 2019", "Health Innovation Fair 2018". "Mexican Mathematics Olympiad 2014"]
   var apps = ["brain.", "DynamicMe"]
 
   var body: some View {
