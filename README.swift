26a3ac7aa4c16eed25c3dd61609031be4b45eece
Juan Luis Flores <juluflosa01.demos@gmail.com>
2024-10-17 14:29:51 -0600

    Update README.swift

--- README.swift
+++ README.swift
@@ -8,13 +8,13 @@
 struct ContentView: View {
 
   // About me
-  let name = "Juan Luis Flores Sánchez"
-  let age = 22
-  let county = "🇲🇽"
+  let name      = "Juan Luis Flores Sánchez"
+  let age       = 23
+  let county    = "🇲🇽"
   var interests = ["Swift", "Apple", "MATLAB", "Python", "Design Thinking"]
-  var awards = ["Swift Student Challenge 2024". "StartUpShip 2023", "Expo Ingenierias 2022", "Conexion Tec 2020", 
-                "PrepApps 2019", "Health Innovation Fair 2018". "Mexican Mathematics Olympiad 2014"]
-  var apps = ["brain.", "DynamicMe"]
+  var awards    = ["Swift Student Challenge 2024". "StartUpShip 2023", "Expo Ingenierias 2022", "Conexion Tec 2020", 
+                  "PrepApps 2019", "Health Innovation Fair 2018". "Mexican Mathematics Olympiad 2014"]
+  var apps      = ["brain.", "DynamicMe"]
 
   var body: some View {
     Text("Hello, world!")
